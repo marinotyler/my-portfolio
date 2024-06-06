49f1e37301c8f75fe6d431381d7bf5d57f37a3c1
Ty Marino <82692393+marinotyler@users.noreply.github.com>
2024-06-05 22:37:21 -0700

    stopped card tilt issue

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,54 +1,61 @@
 /* eslint-disable react/jsx-key */
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import "@/styles.css"
 import Tilt from "react-parallax-tilt"
 import Link from 'next/link'
 import Image from 'next/image'
 
 export default function ProjectsPage() {
+
+    const [tilt, setTilt] = useState(true)
+
     const projects =[
         "project 1",
         "project 2", 
         "project 3", 
         "project 4",
         "project 5", 
-        "project 6" 
+        "project 6"  
     ]   
+
   return (
     <>
         <h1 className='text-6xl'>Projects</h1>
         <div className="container">
+        
             {projects.map((project) =>(
+                <div className='project-container'>
                 <Tilt
-                tiltEnable= {true}
-                perspective={1500}
+                tiltEnable= {tilt}
+                perspective={1000}
                 tiltAngleXInitial={0}
                 tiltAngleYInitial={0}
-                tiltMaxAngleX={15}
-                tiltMaxAngleY={15}
+                tiltMaxAngleX={20}
+                tiltMaxAngleY={20}
                 transitionSpeed={3000}
                 reset = {true}
                 glareEnable={true}
                 glareColor="lightgreen"
                 >
-                    <div className='project-container'>
-                        <div className="project-header text-2xl"> 
+                        <div className="project-card text-2xl"> 
                             <h1>{project}</h1> 
-                        </div>
                             <Link
+                            className="text-xs"
                                 href="https://github.com/marinotyler?tab=repositories">
-                                <Image
-                                className='project-link-icon'
+                                <Image 
+                                className='project-link-icon '
                                 src='/github-icon.png'
                                 width="25"
                                 height="25"
                                 alt='github icon'
-                                />
+                                onMouseOver= {() => setTilt(!tilt)}
+                                onMouseLeave={()=> setTilt(!tilt)}
+                                />Repo
                             </Link>
-                    </div>
-                    
-                </Tilt>
+                        </div>
+                    </Tilt>
+                 </div>
                 
             ))
             }
